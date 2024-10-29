0fec612aaa6d162f59dd18f6676a5ed5d5a2104f
unknown <juegosxepor@gmail.com>
2024-10-28 22:20:05 -0500

    Se uso SINGLETON en la conexion a la base de datos

--- src/java/com/mapas/config/Conexion.java
+++ src/java/com/mapas/config/Conexion.java
@@ -1,4 +1,3 @@
-
 package com.mapas.config;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,17 +6,35 @@ import java.sql.SQLException;
  *
  * @author dkred
  */
-public class Conexion {
-    Connection con;
+public class Conexion
+{
+    Connection con=null;
     String url ="jdbc:mysql://localhost:3306/estacionate";
     String user="root";
     String pass ="";
-    public Connection getConnection(){
-        try {
+    private static Conexion conexion;
+    private Conexion()
+    {
+        this.con=null;
+    }
+    public static Conexion Obtener_Conexion()
+    {
+        if (conexion==null)
+        {
+            conexion=new Conexion();
+        }
+        return conexion;
+    }
+    public Connection Obterner_Conexion()
+    {
+        try
+        {
             Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection(url,user,pass);
-        } catch(ClassNotFoundException | SQLException e){
-            
+        }
+        catch(ClassNotFoundException | SQLException e)
+        {
+            System.out.println("Error en la conexcion con la base de datos");
         }
         return con;
     }
