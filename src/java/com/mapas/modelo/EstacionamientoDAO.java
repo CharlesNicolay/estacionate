4f8865d9cee5eea2b77107ad3fc7856f5d8be516
unknown <juegosxepor@gmail.com>
2024-10-28 22:22:01 -0500

    Se modifico clase Estacionamiento y Estacionamiendo-DAO

--- src/java/com/mapas/modelo/EstacionamientoDAO.java
+++ src/java/com/mapas/modelo/EstacionamientoDAO.java
@@ -1,54 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mapas.modelo;
-
 import com.mapas.config.Conexion;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.http.HttpServletResponse;
-
-/**
- *
- * @author dkred
- */
-public class EstacionamientoDAO {
+public class EstacionamientoDAO
+{
+    Conexion conexion=Conexion.Obtener_Conexion();//LLamamos al metodo Singleton
     Connection con;
-    Conexion cn = new Conexion();
     PreparedStatement ps;
     ResultSet rs;
-    
-    public List listar(){
+    public List listar()
+    {
         List<Estacionamiento> estacionamientos = new ArrayList();
         String sql="select * from estacionamientos";
-        try{
-            con = cn.getConnection();
+        try
+        {
+            con = conexion.Obterner_Conexion();
             ps= con.prepareStatement(sql);
             rs=ps.executeQuery();
-            while(rs.next()){
+            while(rs.next())
+            {
                 Estacionamiento p= new Estacionamiento();
                 p.setId(rs.getInt(1));
                 p.setLatitud(rs.getString(2));
                 p.setLongitud(rs.getString(3));
                 p.setNombre(rs.getString(4));
                 p.setCapacidad(rs.getInt(5));
-                p.setId_caracteristica(rs.getInt(6));
                 estacionamientos.add(p);
             }
-        }catch(Exception e){
-            
         }
-        return estacionamientos;        
-            
-
+        catch(SQLException e)
+        {
+            System.out.println("Error en EstacionamientoDAO_Listar:\n"+e);
+        }
+        return estacionamientos;
     }
     public void listarEstaconamiento(int id, HttpServletResponse response){
         String sql="select * from estacionamientos where id="+id;
@@ -59,17 +53,16 @@ public class EstacionamientoDAO {
         
         try{
             outputStream=response.getOutputStream();
-            con = cn.getConnection();
+            con = conexion.Obterner_Conexion();
             ps= con.prepareStatement(sql);
             rs=ps.executeQuery();
             if(rs.next()){
                 
             }
-        }catch(Exception e){
-            
         }
-              
-            
+        catch(IOException | SQLException e)
+        {
+            System.out.println("Error en Estacionamiento_ListarEstacionamiento:\n"+e);
+        }
     }
-    
 }
